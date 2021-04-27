472cb727c88c6cda34b536767d87935b740e4e73
ritchhai000 <ritchhai000@warren.k12.in.us>
2021-04-27 13:21:45 -0400

    Update

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,15 @@
-public class Board {
+import java.awt.*;
+import javax.swing.*;
 
+public class Board extends JPanel{
+
+    //testing out not using individual classes for these-
+    int player, enemy, bullet;
+
+    public Board(){
+
+        setPreferredSize(new Dimension(600,700));
+        setBackground(Color.WHITE);
+    }
 
 }
--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,30 @@
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame{
+
+    Board board = new Board();
+
+    public Game(){
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        setResizable(false);
+        setTitle("Arcade Game");
+        add(board);
+        pack();
+
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+
+        Game game = new Game();
+
+    }
+
+
+
 
 
 }
