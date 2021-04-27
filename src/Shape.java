959a0ef6062a04c920193539bb3879d4fa568c9b
ritchhai000 <ritchhai000@warren.k12.in.us>
2021-04-27 13:21:53 -0400

    Update

--- src/Shape.java
+++ src/Shape.java
@@ -1,4 +1,14 @@
+import java.awt.*;
+
 public class Shape {
+
+    int width, height, x, y;
+    double dx, dy;
+
+    public Shape(Color color, int x, int y, int width, int height){
+
+
+    }
 
 
 }
