a361d7b7d5f8bb03f5b9a80b3ac632c805847c70
ritchhai000 <ritchhai000@warren.k12.in.us>
2021-04-29 13:22:22 -0400

    Update

--- src/Board.java
+++ src/Board.java
@@ -1,15 +1,35 @@
 import java.awt.*;
+import java.util.ArrayList;
 import javax.swing.*;
 
 public class Board extends JPanel{
 
     //testing out not using individual classes for these-
-    int player, enemy, bullet;
+    Color color;
+    int enemy, bullet;
+
+    ArrayList<Shape> player = new ArrayList<>();
+    Shape play = new Shape(Color.RED, 400, 400, 400, 400);
 
     public Board(){
 
         setPreferredSize(new Dimension(600,700));
         setBackground(Color.WHITE);
     }
+
+    public void Play(Color color, int x, int y, int width, int height){
+
+    }
+
+    public setup(){
+
+
+    }
+
+    public void setPlayPos(int x, int y){
+
+        player.get(0).setPos(x, y);
+    }
+
 
 }
--- src/Shape.java
+++ src/Shape.java
@@ -2,13 +2,27 @@ import java.awt.*;
 
 public class Shape {
 
+    Color color;
     int width, height, x, y;
     double dx, dy;
 
     public Shape(Color color, int x, int y, int width, int height){
 
-
+        this.color = color;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
     }
+
+    public void setPos(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setColor(Color color){this.color = color;}
+    public void paint(Graphics p){}
+
 
 
 }
